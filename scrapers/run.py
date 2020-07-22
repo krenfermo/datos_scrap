fc676288a03221b08635fe179a04a17464a51ad1
krenfermo <morakurt@gmail.com>
2020-07-21 20:20:52 -0500

    agrego ADLATINA

--- scrapers/run.py
+++ scrapers/run.py
@@ -1,6 +1,7 @@
 from datos_computrabajo import computrabajo
 from datos_zonajobs import zonajobs
 from datos_linkedin import linkedin
+from datos_adlatina import adlatina
 import sys
 #if sys.argv[1]:
 zona=0
@@ -30,6 +31,17 @@ try:
         pass
     else:
         job=linkedin()
+            
+except Exception as ex:
+    print(ex)
+
+
+try:
+    job=adlatina()
+    if job==0:
+        pass
+    else:
+        job=adlatina()
             
 except Exception as ex:
     print(ex)
